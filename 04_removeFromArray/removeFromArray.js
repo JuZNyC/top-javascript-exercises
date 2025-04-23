8236f92b6d79e8b52fd6232c5b10f79149ace24e
JuZNyC <61894460+JuZNyC@users.noreply.github.com>
2025-04-23 15:16:58 -0400

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = [];
+    array.forEach(element => {
+        if (!args.includes(element)) {
+            result.push(element);
+        }
+    });
+
+    return result;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// removeFromArray([1, 3, 3, 4], 3);
