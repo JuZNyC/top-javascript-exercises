2164a671d77cb65d479d7564f6c554b66b6b3159
JuZNyC <61894460+JuZNyC@users.noreply.github.com>
2025-04-23 15:27:39 -0400

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (Number.isInteger(numOne + numTwo) && numOne * numTwo >= 0) {
+        let start;
+        let end;
+        if (numOne > numTwo) {
+            start = numTwo;
+            end = numOne;
+        }
+        else {
+            start = numOne;
+            end = numTwo;
+        }
+        result = 0;
+        for (let i = start; i <= end; i++) {
+            result += i;
+        }
+        return result;
+    }
+    else {
+        return "ERROR"
+    }
 
-};
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
