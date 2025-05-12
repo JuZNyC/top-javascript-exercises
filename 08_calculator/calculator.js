7b8f8953312dc23cd0cd06757e04f37d8b46f81e
JuZNyC <61894460+JuZNyC@users.noreply.github.com>
2025-05-12 19:01:57 -0400

    completed 09_palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum = 0;
+  for (i in a) {
+    sum += a[i];
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let total = 1;
+  for (i in a) {
+    total *= a[i];
+  };
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  if (a === 0 || a === 1) {
+    return total;
+  }
+	for (let i = a; i >= 2; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    let cleaned = a.toString();
+    cleaned = cleaned.replace(/[^\w\s]/g, '')
+    cleaned = cleaned.replaceAll(" ", "");
+    cleaned = cleaned.trim();
+    cleaned = cleaned.toLowerCase();
+    firstHalf = cleaned.slice(0, cleaned.length / 2);
+    secondHalf = cleaned.slice(cleaned.length / 2, cleaned.length).split("").reverse().join("");
 
+    for (let i = 0; i < firstHalf.length; i++) {
+        if(firstHalf[i] != secondHalf[i]) {
+            return false;
+        }
+    };
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+palindromes('A car, a man, a maraca.');
