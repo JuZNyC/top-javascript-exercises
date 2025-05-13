cd3f6dbab077dde1cb77f43ede3eade1d289a04d
JuZNyC <61894460+JuZNyC@users.noreply.github.com>
2025-05-13 14:58:41 -0400

    reinstalling fedora

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let num = parseInt(a);
+    let fib = [1,1];
 
+    if (num === 0) {
+        return 0;
+    };
+    if (num === 1 || num === 2) {
+        return 1;
+    };
+
+    if (num < 0) {
+        return "OOPS";
+    }
+    
+    for (let i = 2; i < num; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    };
+    return fib[num - 1];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+    let oldestAge = 0;
+    let result = {};
+    a.forEach(element => {
+        let age = element.yearOfDeath - element.yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            result = element;
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line
